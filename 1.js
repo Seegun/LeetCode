b39aa4227282828a738828cd5e328cfe9b312266
Seegun <50489396+Seegun@users.noreply.github.com>
2023-10-16 18:50:34 +0300

    Update and rename 1 to 1.js

---
+++ 1.js
@@ -0,0 +1,37 @@
+// Задача: https://leetcode.com/problems/create-hello-world-function/?envType=study-plan-v2&envId=30-days-of-javascript
+// Код который был на сайте:
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+       
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+// Моё решение: 
+
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+       return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+// Как решал:
+// Ну тут я даже не знаю что написать 
+// попросили сделать так чтобы функция возвращала всегда "Hello World" сделал ретерн в функции и вернул)
