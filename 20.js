fdfdefc3b386f8af66d99b8602767fb53d5120fc
Seegun <50489396+Seegun@users.noreply.github.com>
2023-10-24 20:39:56 +0300

    Create 20.js

---
+++ 20.js
@@ -0,0 +1,28 @@
+// Задача: https://leetcode.com/problems/is-object-empty/?envType=study-plan-v2&envId=30-days-of-javascript
+// Код на сайте: 
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    
+};
+
+// Моё решение:
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if (Object.keys(obj).length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+};
+
+// как решал:
+// тут проверка если объект пустой то верну true если не пустой false
