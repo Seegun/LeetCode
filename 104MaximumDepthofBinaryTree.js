7a94d34aa834c0b4e9f17f018b2f3cb596c484db
Seegun <50489396+Seegun@users.noreply.github.com>
2023-11-23 16:15:39 +0300

    Create 104MaximumDepthofBinaryTree.js

---
+++ 104MaximumDepthofBinaryTree.js
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/?envType=study-plan-v2&envId=top-interview-150
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) {
+        return 0;
+    }
+
+    var leftDepth = maxDepth(root.left);
+    var rightDepth = maxDepth(root.right);
+
+    return Math.max(leftDepth, rightDepth) + 1;
+};
